fc90a736196fc0e640b6a0e42a8eaf458ec70e4c
KamilMachno <kmachno@gmail.com>
2019-09-01 13:22:44 -0700

    Dodanie obslugi klikniec i sprawdzania

--- Memory/MainWindow.xaml.cs
+++ Memory/MainWindow.xaml.cs
@@ -21,8 +21,8 @@ namespace Memory
     public partial class MainWindow : Window
     {
         private CardType[] cardTypes;
-        private string move1;
-        private string move2;
+        private bool move1 = true;
+
         //lista znakow, ktore beda na kartach
         private List<char> signs;
 
@@ -65,6 +65,62 @@ namespace Memory
                 button.Foreground = button.Background;
             });
         }
+        /// <summary>
+        /// Resetowanie planszy
+        /// </summary>
+        private void RefreshPlay()
+        {
+            Board_grid.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                var column = Grid.GetColumn(button);
+                var row = Grid.GetRow(button);
+
+                if (cardTypes[column + row * 5] == CardType.click)
+                {
+                    cardTypes[column + row * 5] = CardType.free;
+                    button.Background = Brushes.White;
+                    button.Foreground = button.Background;
+                }
+            });
+        }
+
+        /// <summary>
+        /// Sprawdzanie czy zostala odkryta para takich samych znakow
+        /// </summary>
+        private void CheckHit()
+        {
+            List<Button> hittedbtn = new List<Button>();
+            List<int> cards = new List<int>();
+
+            Board_grid.Children.Cast<Button>().ToList().ForEach(button =>
+            {
+                var column = Grid.GetColumn(button);
+                var row = Grid.GetRow(button);
+
+                if (cardTypes[column + row * 5] == CardType.click)
+                {
+                    hittedbtn.Add(button);
+                    cards.Add(column + row * 5);
+                }
+            });
+
+            if(hittedbtn.Count == 2)
+            {
+                
+                if (hittedbtn[0] == hittedbtn[1])
+                {
+                    cardTypes[cards[0]] = CardType.hit;
+                    cardTypes[cards[1]] = CardType.hit;
+                }
+            }
+
+            RefreshPlay();
+            
+ 
+
+
+        }
+        
 
         private void Gamebtn(object sender, RoutedEventArgs e)
         {
@@ -74,6 +130,23 @@ namespace Memory
 
         private void Cardbtn(object sender, RoutedEventArgs e)
         {
+            //Wyswietlenia tylko 2 elementow 
+            if (move1)
+            {
+                CheckHit();
+            }
+            move1 ^= true;
+
+            Button bt1 = (Button)sender;
+
+            //Wspolzedne kliknietego button
+            var column = Grid.GetColumn(bt1);
+            var row = Grid.GetRow(bt1);
+
+            cardTypes[column + row * 5] = CardType.click;
+
+            bt1.Background = Brushes.Gray;
+            
 
         }
     }
